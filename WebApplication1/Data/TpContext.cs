724cd7f8ff7e6c23164696793a01a0ffa5cccdb7
David Tielke <mail@david-tielke.de>
2016-06-28 10:57:38 +0200

    Fix EF Push

---
+++ WebApplication1/Data/TpContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApplication1.Data.Mappings;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class TpContext : DbContext
+    {
+        public TpContext()
+            : base("name=TpContext")
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<TpContext>());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ParticipantsMappings());
+        }
+
+        public DbSet<Participant> Participants { get; set; }
+    }
+}
