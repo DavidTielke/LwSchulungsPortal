5b97c48b7c69962c393e4b4fbd9e44033fd76c0e
David Tielke <mail@david-tielke.de>
2016-06-29 13:38:17 +0200

    Partial View mit Menüs hinzugefügt

--- WebApplication1/Migrations/Configuration.cs
+++ WebApplication1/Migrations/Configuration.cs
@@ -17,10 +17,6 @@ namespace WebApplication1.Migrations
 
         protected override void Seed(WebApplication1.Data.TpContext context)
         {
-            context.Participants.AddOrUpdate(new Participant("David", "Tielke", "mail@david-tielke.de", "http://www.David-Tielke.de", "www.David-Tielke.de"));
-            context.Participants.AddOrUpdate(new Participant("Golo", "Roden", "webmaster@goloroden.de", "http://www.thenativeweb.de", "theNativeWeb"));
-            context.Participants.AddOrUpdate(new Participant("Laurin", "Stoll", "l.stoll@yooapps.ch", "http://www.yooapps.ch", "youuapps AG"));
-            context.Participants.AddOrUpdate(new Participant("Christian", "Giesswein", "info@giessweinweb.at", "http://www.giessweinweb.at", "Giesswein Apps"));
         }
     }
 }
