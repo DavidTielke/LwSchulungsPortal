0b443fa658151d81bf27e6d3e5b0f338ac01a4cb
David Tielke <mail@david-tielke.de>
2016-06-30 15:20:18 +0200

    MyPrincipal hinzugefügt

--- WebApplication1/Global.asax.cs
+++ WebApplication1/Global.asax.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using WebApplication1.Data;
+using WebApplication1.Models;
 
 namespace WebApplication1
 {
@@ -13,6 +16,41 @@ namespace WebApplication1
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
+        {
+            var isUserAuthenticated = User != null && User.Identity.IsAuthenticated;
+            if (isUserAuthenticated)
+            {
+                var repo = new Repository<Participant>();
+                var user = repo.Query.Single(p => p.Email == User.Identity.Name);
+                HttpContext.Current.User = new MyPrincipal(User.Identity.Name)
+                {
+                    Firstname = user.Firstname,
+                    Lastname = user.Lastname,
+                    Id = user.Id
+                };
+            }
+        }
+    }
+
+    public class MyPrincipal : IPrincipal
+    {
+        public bool IsInRole(string role)
+        {
+            return false;
+        }
+
+        public IIdentity Identity { get; }
+
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Id { get; set; }
+
+        public MyPrincipal(string email)
+        {
+            Identity = new GenericIdentity(email, "forms");
         }
     }
 }
