eef145c156aed9c0b2ab515fd7303357b0752b4d
David Tielke <mail@david-tielke.de>
2016-06-30 15:30:13 +0200

    MyViewPage hinzugefügt

---
+++ WebApplication1/Views/MyViewPage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Views
+{
+    public abstract class MyViewPage : WebViewPage
+    {
+        public new MyPrincipal User
+        {
+            get { return (MyPrincipal)base.User; }
+        }
+
+        public bool IsInRole(string role)
+        {
+            return User.IsInRole(role);
+        }
+    }
+    public abstract class MyViewPage<T> : WebViewPage<T>
+    {
+        public new MyPrincipal User
+        {
+            get { return (MyPrincipal)base.User; }
+        }
+
+        public bool IsInRole(string role)
+        {
+            return User.IsInRole(role);
+        }
+    }
+}
