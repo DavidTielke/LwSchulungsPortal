f4928753ae383513e75c3e0aaf9700090747ee49
David Tielke <mail@david-tielke.de>
2016-06-30 11:02:31 +0200

    Views für Login hinzugeügt

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Logon
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AccountLoginViewModel viewModel)
+        {
+            var isValid = ModelState.IsValid;
+            if (isValid)
+            {
+                
+            }
+            return View(viewModel);
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(AccountRegisterViewModel viewModel)
+        {
+            var isValid = ModelState.IsValid;
+            if (isValid)
+            {
+                
+            }
+            return View(viewModel);
+        }
+    }
+}
---
+++ WebApplication1/ViewModels/AccountRegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.ViewModels
+{
+    public class AccountRegisterViewModel
+    {
+        [Required]
+        [MinLength(3)]
+        public string Email { get; set; }
+        [UIHint("Password")]
+        [MinLength(5)]
+        public string Password { get; set; }
+        [UIHint("Password")]
+        [MinLength(5)]
+        public string PasswordRepeat { get; set; }
+    }
+}
